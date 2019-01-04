6f979f34e21072339c883e1ea6a1900d3e5b034a
孙尚武 <18712858087@qq.com>
2019-01-04 17:50:19 +0800

    123

--- src/JwtAuth.php
+++ src/JwtAuth.php
@@ -18,18 +18,58 @@ class JwtAuth extends JWT
      * @param $data
      * @return false|string
      */
-    public function createToken($model, $data)
+    public static function createToken($model, $data)
     {
+        session_start();
+
         if (empty($data) || empty($model)) {
             return json_encode(array('code' => -2, 'message' => "参数不能为空"));
         }
         //获取用户标识
         $identifier = config()['jwt']['identifier'];
         $key = config()['jwt']['key'];
+        $getUser = $model->where($identifier, '=', $data[$identifier])->find();
 
-
+        if (empty($getUser)) {
+            return json_encode(array('code' => -2, 'message' => '无法生成token'));
+        }
+        $token = [
+            'iss' => config()['jwt']['iss'], //签发者 可选
+            'aud' => config()['jwt']['aud'], //接收该JWT的一方，可选
+            'iat' => config()['jwt']['iat'], //签发时间
+            'nbf' => config()['jwt']['nbf'], //(Not Before)：某个时间点后才能访问，比如设置time+30，表示当前时间30秒后才能使用
+            'exp' => config()['jwt']['exp'], //过期时间,这里设置2个小时
+            'data' => $data
+        ];
+        $_SESSION[config()['jwt']['session_key']] = JWT::encode($token, $key);
+        return json_encode(array('code' => 1, 'message' => JWT::encode($token, $key)));
 
     }
 
+    /**
+     * 验证token
+     * @param $token
+     * @return false|string
+     */
+    public static function validateToken($token)
+    {
 
+        $key = config()['jwt']['key'];
+        try {
+            JWT::$leeway = 60;
+            JWT::decode($token, $key, ['HS256']);
+            if ($token != $_SESSION[config()['jwt']['session_key']]) {
+                return json_encode(array('code' => -2, 'message' => 'token is error'));
+            }
+
+        } catch (SignatureInvalidException $e) {//签名不正确
+            return json_encode(array('code' => -2, 'message' => $e->getMessage()));
+        } catch (BeforeValidException $e) {//签名在某个时间点之后才能用
+            return json_encode(array('code' => -2, 'message' => $e->getMessage()));
+        } catch (ExpiredException $e) {//token过期
+            return json_encode(array('code' => -2, 'message' => $e->getMessage()));
+        } catch (\Exception $e) {//其他错误
+            return json_encode(array('code' => -2, 'message' => $e->getMessage()));
+        }
+    }
 }
